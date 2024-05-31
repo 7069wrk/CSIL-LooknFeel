ae369ca1b1e1076f5f6624196de38e7db3473386
7069wrk <102612356+7069wrk@users.noreply.github.com>
2024-05-31 09:17:11 -0500

    Update csil_bootloader.sh

--- csil_bootloader.sh
+++ csil_bootloader.sh
@@ -10,11 +10,11 @@ output_file="/tmp/csil_themes-$timestamp.log"
 ### | tee -a "$output_file"
 
 echo "# Restore the backup of CSI Theme..."  | tee -a "$output_file"
-echo $key | sudo -S tar --overwrite -xpf "csi_boot.tar" -C /;
+local tar_file="csi_boot.tar"
+echo $key | sudo -S tar --overwrite -xpf "$tar_file" -C /;
 
 sleep 5
 echo "# Installing the CSI BOOTLOADER Theme..."  | tee -a "$output_file"
-
 
 ###DOING GRUB STUFF
 echo "Doing Grub stuff..."
@@ -25,30 +25,38 @@ if echo $key | sudo -S grep -q "GRUB_DISABLE_OS_PROBER=false" /etc/default/grub;
     echo "Grub is already configured for os-probe" | tee -a "$output_file"
 fi
 
+echo $key | sed -i 's/GRUB_TIMEOUT_STYLE=hidden/GRUB_TIMEOUT_STYLE=menu #hidden/g' /etc/default/grub
+
 if ! echo $key | sudo -S grep -q "GRUB_THEME=/boot/grub/themes/csilinux1/theme.txt" /etc/default/grub; then		
   echo $key | sudo -S sed -i '$a GRUB_THEME=/boot/grub/themes/csilinux1/theme.txt' /etc/default/grub
   echo "Grub is already configured for CSI Theme" | tee -a "$output_file"
 fi
 
 ###SEARCHES for the timeout and replaces the time with 5
-echo $key | sudo -S sed -i 's/GRUB TIMEOUT="[^"]*"/GRUB TIMEOUT="5"/' /etc/default/grub
+echo $key | sudo -S sed -i 's/GRUB TIMEOUT="[^"]*"/GRUB TIMEOUT="10"/' /etc/default/grub
 
-echo $key | sudo -S sed -i '$a GRUB_GFXMODE=1024x768x16' /etc/default/grub
-echo $key | sudo -S sed -i '$a GRUB_GFXPAYLOAD_LINUX=keep' /etc/default/grub
 #GRUB_GFXMODE="1024x768x16"
+if ! echo $key | sudo -S grep -q "GRUB_GFXMODE=1024x768x16" /etc/default/grub; then		
+  echo $key | sudo -S sed -i '$a GRUB_GFXMODE=1024x768x16' /etc/default/grub
+  echo "Grub_GFXMODE is already configured for CSI Theme" | tee -a "$output_file"
+fi
+
 #GRUB_GFXPAYLOAD_LINUX="keep"
-
-
+if ! echo $key | sudo -S grep -q "GRUB_GFXPAYLOAD_LINUX=keep" /etc/default/grub; then		
+  echo $key | sudo -S sed -i '$a GRUB_GFXPAYLOAD_LINUX=keep' /etc/default/grub
+  echo "Grub_GFXPAYLOAD is already configured for CSI Theme" | tee -a "$output_file"
+fi
 
 ###SEARCHES for the command and replaces all `1` with `0`
 echo $key | sudo -S sed -i '/recordfail_broken=/{s/1/0/}' /etc/grub.d/00_header
 
+sleep 5
+echo $key | sudo -S update-grub	
+
 ###LINES that need to be written or can be - the INIT_TUNE is not required.
+#GRUB TIMEOUT=10
 #GRUB_DISABLE_OS_PROBER="false"
 #GRUB_GFXMODE="1024x768x16"
 #GRUB_GFXPAYLOAD_LINUX="keep"
 ##GRUB_INIT_TUNE="480 440 1"
 #GRUB_THEME="/boot/grub/themes/csilinux1/theme.txt"
-
-sleep 5
-echo $key | sudo -S update-grub	
