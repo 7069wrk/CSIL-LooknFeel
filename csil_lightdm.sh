b3cb139cd59114b48edb35708a8b012bf8daacce
7069wrk <102612356+7069wrk@users.noreply.github.com>
2024-06-01 19:58:09 -0500

    Add files via upload

---
+++ csil_lightdm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# GET SUDO
+echo "Please enter your sudo password:"
+read -s key
+
+# LOG FOR NEW CSIL SYSTEM
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+output_file="csil_lightdm-$timestamp.log"
+### | tee -a "$output_file"
+
+echo "Add LIGHTDM to CSI Theme..." | tee -a "$output_file"
+tar_file="csi_lightdm.tar"
+echo $key | sudo -S tar --overwrite -xpf "$tar_file" -C /
+
+sleep 5
+echo "# Installing the CSI LIGHTDM login Theme..." | tee -a "$output_file"
+
+sudo systemctl restart lightdm
