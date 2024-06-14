2f9fe7580fe9601ae4be1d0c31c07f28ea03589e
7069wrk <102612356+7069wrk@users.noreply.github.com>
2024-06-14 13:40:00 -0500

    Create csil_slim.sh

---
+++ csil_slim.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# GET SUDO
+echo "Please enter your sudo password:"
+read -s key
+
+# LOG FOR NEW CSIL SYSTEM
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+output_file="csil_slim-$timestamp.log"
+### | tee -a "$output_file"
+
+echo "Add LOGIN to CSI Theme..." | tee -a "$output_file"
+tar_file="csi_loooknfeel.tar"
+echo $key | sudo -S tar --overwrite -xpf "$tar_file" -C /
+
+# Check root privileges
+if [[ $(id -u) != 0 ]] ; then
+  echo "This script must be run with root privileges."
+  exit 1
+fi
+
+# Update package list
+apt update
+
+# Install SLIM
+apt install slim -y
+
+# Backup slim config
+cp -v /etc/slim.conf /etc/slim.conf.org  # -v for verbose output
+
+# Define custom configuration content (replace with your actual configuration)
+CUSTOM_CONFIG=$(cat <<EOF
+### CUSTOM CSIL SLIM.CONF
+default_path  /usr/local/bin:/usr/bin:/bin:/opt/csitools:/opt/csitools/helper:/home/csi/bin:/home/csi/.local/bin:/usr/sbin
+default_xserver  /usr/bin/X11/X
+xserver_arguments  -nolisten tcp
+halt_cmd        /sbin/shutdown -h now
+reboot_cmd      /sbin/shutdown -r now
+console_cmd     /usr/bin/xterm -C -fg white -bg black +sb -T "Console login" -e /bin/sh -c "/bin/cat /etc/issue.net; exec /bin/login"
+xauth_path      /usr/bin/X11/xauth
+authfile        /var/run/slim.auth
+numlock          on
+login_cmd       exec /bin/bash -login /etc/X11/Xsession %session
+sessiondir      /usr/share/xsessions/
+screenshot_cmd  scrot /root/slim.png
+welcome_msg     Welcome to CSI Linux
+shutdown_msg    The system is halting...
+reboot_msg      The system is rebooting...
+default_user     csi
+focus_password  yes
+auto_login       no   
+current_theme    csi2
+lockfile        /var/run/slim.lock
+logfile         /var/log/slim.log
+EOF
+)
+
+# Check if custom config content is empty
+if [[ -z "$CUSTOM_CONFIG" ]]; then
+  echo "Custom configuration is empty. Please define content."
+  exit 1
+fi
+
+# Write custom configuration to a new slim.conf file
+echo "$CUSTOM_CONFIG" > /etc/slim.conf
+
+# Restart SLIM service (optional)
+systemctl restart slim
+
+echo "SLIM installed and new configuration file created."
