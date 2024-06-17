865bf10f729d1228c432b734f7b09a184dfae3d5
7069wrk <102612356+7069wrk@users.noreply.github.com>
2024-06-17 06:44:36 -0500

    Update 4_csil_wallpaper.sh

--- 4_csil_wallpaper.sh
+++ 4_csil_wallpaper.sh
@@ -33,3 +33,6 @@ sleep 5
 echo "# Installing the CSI WALLPAPER Theme..." #| tee -a "$output_file"
 
 update_xfce_wallpapers "/opt/csitools/wallpaper/CSI-Linux-Dark.jpg"
+
+### single desktop wallpaper
+#gsettings set org.gnome.desktop.background picture-uri file://///opt/csitools/wallpaper/CSI-Linux-Dark.jpg  
