b79eed86656fb535c7977c269407ae594e614c71
7069wrk <102612356+7069wrk@users.noreply.github.com>
2024-06-18 12:16:44 -0500

    Update 0_csil_looknfeel.sh

--- 0_csil_looknfeel.sh
+++ 0_csil_looknfeel.sh
@@ -18,9 +18,9 @@ output_file="/usr/share/.logs/csil-looknfeel-$timestamp.log"
 ### | tee -a "$output_file"
 
 ### add repositories
-sudo add-apt-repository universe -y
-sudo add-apt-repository multiverse -y
-sudo add-apt-repository restricted -y
+sudo add-apt-repository universe -y | tee -a "$output_file"
+sudo add-apt-repository multiverse -y | tee -a "$output_file"
+sudo add-apt-repository restricted -y | tee -a "$output_file"
 
 #sudo add-apt-repository --remove universe
 #sudo add-apt-repository --remove multiverse
@@ -30,30 +30,30 @@ sudo add-apt-repository restricted -y
 sudo apt update
 
 ### install build dependencies
-echo "Installing VM TOOLS"
+echo "Installing VM TOOLS" | tee -a "$output_file"
 sudo apt install -y open-vm-tools open-vm-tools-desktop
-echo "installing PYTHON STUFF"
+echo "installing PYTHON STUFF" | tee -a "$output_file"
 sudo apt install -y python3 python3-pip python3-venv python3-update-manager
-echo "installing FILE TRANSPORTATION"
+echo "installing FILE TRANSPORTATION" | tee -a "$output_file"
 sudo apt install -y git curl wget
-echo "installing COMPRESSION UTILITIES"
+echo "installing COMPRESSION UTILITIES" | tee -a "$output_file"
 sudo apt install -y p7zip-full p7zip-rar zip
-echo "installing UTILS"
+echo "installing UTILS" | tee -a "$output_file"
 sudo apt install -y aria2 bpytop yad zenity dos2unix
 sleep 5
-echo "installing DESKTOP TRANSFORMATIONS"
+echo "installing DESKTOP TRANSFORMATIONS" | tee -a "$output_file"
 sudo apt install -y xfce4 xfce4-goodies gvfs-backends dbus-x11 task-xfce-desktop
 #sudo apt install -y tasksel xubuntu-desktop task-xfce-desktop
 sudo apt install -y figlet
 sleep 5
-echo "installing SLIM"
+echo "installing SLIM" | tee -a "$output_file"
 sudo apt install -y slim
 
 ### git the repository
 #echo $key | sudo -S git clone https://github.com/7069wrk/CSIL-LooknFeel.git
 
 ### restore to root
-echo "expanding CSIL ROOT"
+echo "expanding CSIL ROOT" | tee -a "$output_file"
 tar_file="csi_looknfeel.tar"
 echo $key | sudo -S tar --overwrite -xpf "$tar_file" -C /
 
@@ -70,11 +70,13 @@ echo $key | sudo -S tar --overwrite -xpf "$tar_file" -C /
 #source 3_csil--wallpaper
 
 #power and security
+echo "removing screen and power LOCKS" | tee -a "$output_file"
+sudo -k
 gsettings set org.gnome.desktop.session idle-delay 9999
 gsettings set org.gnome.desktop.screensaver lock-delay 9999
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 
-
+echo "we thing we are done now you can move on to the next step" | tee -a "$output_file"
 
 
 #  mapfile -t csi_linux_themes < <(grep -vE "^\s*#|^$" csi_linux_themes.txt | sed -e 's/#.*//')
