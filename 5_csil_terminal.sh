7034b5a829ebe7c4cfe4ae2a3313a8469cfc4aed
7069wrk <102612356+7069wrk@users.noreply.github.com>
2024-06-18 12:09:47 -0500

    Update 5_csil_terminal.sh

--- 5_csil_terminal.sh
+++ 5_csil_terminal.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
 
 ### Add the CSI Linux tag to top right of terminal
+# GET SUDO
+echo "Please enter your sudo password:"
+read -s key
+
+# LOG FOR NEW CSIL SYSTEM
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+output_file="/usr/share/.logs/csil_terminal-$timestamp.log"
+### | tee -a "$output_file"
 
 # install FIGLET
-sudo apt install figlet
+echo "installing FIGLET" | tee -a "$output_file"
+echo $key | sudo -S apt install figlet
 
 # get FIGLET fonts
-sudo git clone https://github.com/xero/figlet-fonts /usr/share/figlet/fonts
+echo $key | sudo -S git clone https://github.com/xero/figlet-fonts /usr/share/figlet/fonts
 
 # edit .bashrc
-cd ~/ 
+#exit sudo
+sudo -k
+
 #echo "figlet -f /usr/share/figlet/fonts/Digital -r \"CSI Linux\"" >> ~/.bashrc
 sed -i '$ a figlet -f /usr/share/figlet/fonts/Digital -r "CSI Linux"' ~/.bashrc
 
 # set TERMINAL theme
 # Text color (change '33' for different colors)
-text_color="\033[33m"  
+#text_color="\033[33m"  
 
 # Background color (change '44' for different colors)
-background_color="\033[44m"  
+#background_color="\033[44m"  
 
 # Reset color at the end
-reset_color="\033[0m"
+#reset_color="\033[0m"
 
-clear  # Optional: Clear the screen
+#clear  # Optional: Clear the screen
 
 # Set your preferred prompt style
-PS1="$text_color\u@\h: \w > $reset_color" 
+#PS1="$text_color\u@\h: \w > $reset_color" 
 
 # Apply colors and prompt
-echo -e "$background_color$text_color Welcome to your Custom Terminal! $reset_color"
+#echo -e "$background_color$text_color Welcome to your Custom Terminal! $reset_color"
 
 # Your other commands here...
 
