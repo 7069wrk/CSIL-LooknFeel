b643740b6a8e3ea6f6616f2ec160b5837e4dcff4
7069wrk <102612356+7069wrk@users.noreply.github.com>
2024-06-21 07:10:47 -0500

    Update 1_csil_bootloader.sh

--- 1_csil_bootloader.sh
+++ 1_csil_bootloader.sh
@@ -6,9 +6,9 @@ read -s key
 
 # LOG FOR NEW CSIL SYSTEM
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-output_file="/usr/share/.logs/csil_themes-$timestamp.log"
+output_file="$HOME/csil_themes-$timestamp.log"
 touch "$output_file"
-### | tee -a "$output_file"
+echo $key | sudo -S chmod 7777 "$output_file"### | tee -a "$output_file"
 
 echo "# Restore the backup of CSI Theme..." | tee -a "$output_file"
 #tar_file="csi_boot.tar"
