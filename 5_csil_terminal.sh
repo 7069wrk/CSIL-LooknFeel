9e3ef15e9a0ef6dc00ffa1350c326f2f7c37e050
7069wrk <102612356+7069wrk@users.noreply.github.com>
2024-06-21 10:31:26 -0500

    Update 5_csil_terminal.sh

--- 5_csil_terminal.sh
+++ 5_csil_terminal.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
 ### Add the CSI Linux tag to top right of terminal
+
 # GET SUDO
-echo "Please enter your sudo password:"
-read -s key
+#echo "Please enter your sudo password:"
+#read -s key
+
+# Password file path
+password_file=".passwd"
+
+# Read password securely (avoid storing in script)
+if [[ -f "$password_file" ]]; then
+  # Read first line of password file (assuming password is on the first line)
+  key=$(head -n 1 "$password_file")
+  echo $key
+else
+  echo "Password file not found: $password_file"
+  exit 1  # Script exits with error (no password)
+fi
 
 # LOG FOR NEW CSIL SYSTEM
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-output_file="$HOME/csil_terminal-$timestamp.log"
+output_file="csil_terminal-$timestamp.log"
 touch "$output_file"
-echo $key | sudo -S chmod 7777 "$output_file"### | tee -a "$output_file"
+#echo $key | sudo -S chmod 7777 "$output_file"### | tee -a "$output_file"
 
 # install FIGLET
 echo "installing FIGLET" | tee -a "$output_file"
